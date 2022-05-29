734331e58b8ef567353de90c52751621730e5eac
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-05-29 12:05:18 +0300

    fixing

--- src/pages/CreatePage.js
+++ src/pages/CreatePage.js
@@ -1,4 +1,4 @@
-import {MDBInput, MDBRow} from "mdb-react-ui-kit";
+import {MDBInput, MDBRow,MDBBtn} from "mdb-react-ui-kit";
 import React, {useContext, useState} from "react";
 import {useHistory} from "react-router-dom";
 import {AuthContext} from "../context/AuthContext.js";
@@ -10,18 +10,19 @@ import { ToastContainer, toast } from 'react-toastify';
 export const CreatePage = ()=> {
 	const history = useHistory()
 	const auth = useContext(AuthContext)
-	const {request} = useHttp();
+	const {request,loading} = useHttp();
 	const [link,setLink] = useState('');
 
-	const pressHandler = async event => {
-		if(event.key === 'Enter'){
+	const pressHandler = async (event) => {
+			event.preventDefault();
 			try{
 				const {newLink} = await request(
 					`${process.env.REACT_APP_PUBLIC_LINK}/api/v1/link/generate`,
 					'POST',
 					{from:link},
 					{Authorization:`Bearer ${auth.token}`}
-				)
+				);
+				console.log(loading);
 				toast.success('Link Successfully added');
 				if(newLink){
 					history.push(`/detail/${newLink._id}`);
@@ -29,7 +30,6 @@ export const CreatePage = ()=> {
 			} catch (e) {
 				toast.error(e.message);
 			}
-		}
 	}
 	return (
 		<MDBRow>
@@ -41,9 +41,18 @@ export const CreatePage = ()=> {
 					size='lg'
 					value={link}
 					onChange={e=>setLink(e.target.value)}
-					onKeyPress={pressHandler}
+
 					label="Insert Your Link"
 					/>
+				<MDBBtn
+					type="button"
+					className='my-2'
+					rounded
+					color='info'
+					size='lg'
+					disabled={false}
+					onClick={pressHandler}
+				>Generate</MDBBtn>
 				<ToastContainer/>
 			</div>
 		</MDBRow>
