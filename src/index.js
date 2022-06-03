d046447027d95d4af71d2449c480e373fea45949
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-06-03 13:38:26 +0300

    redesign with material UI

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from "react-router-dom";
 
 import './index.css';
 import App from './App';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
 
-root.render(
-    <Router>
-        <App tab="home" />
-    </Router>
-);
+
+ReactDOM.render(<Router><App /></Router>, container);
